175bfcbde85315a080eff8c570ef409b074da977
Bhavya Jain <jainbhavya0527@gmail.com>
2026-06-16 13:38:45 +0530

    programming element level 1

---
+++ src/gcr_codebase/programming_elements/Level1/HarryAge.java
@@ -0,0 +1,10 @@
+package gcr_codebase.programming_elements.Level1;
+
+public class HarryAge {
+    public static void main(String[] args){
+        int birthyear=1976;
+        int curryear=2025;
+        int age=curryear-birthyear;
+        System.out.println("Harry's age in 2024 is " +age);
+    }
+}
